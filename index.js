f3a05c6d2b8f0e2f24f8424e2e6a8e03802ead1f
Bala Venkatesh Koripalli <bkoripal@opentext.com>
2022-07-01 16:52:07 +0530

    service-discovery-options module error in linux

---
+++ index.js
@@ -0,0 +1,21 @@
+/// <reference types="@vertx/core" />
+// @ts-check
+
+const { HttpEndpoint, ServiceDiscovery } = require('@vertx/service-discovery')
+const { ServiceDiscoveryOptions } = require('@Vertx/service-discovery/options')
+
+const discovery = ServiceDiscovery.create(
+vertx,
+new ServiceDiscoveryOptions({
+name: 'test'
+})
+)
+
+vertx
+  .createHttpServer()
+  .requestHandler(function (req) {
+    req.response().end("Hello ES4X!");
+  })
+  .listen(3000);
+
+console.log('Server started on port 3000');
